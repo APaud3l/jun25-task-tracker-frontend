83c1d1b378be8e29f32dcbfba62d1618ac553d98
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-20 12:26:55 +1100

    feat: Implemented the Login and Register Pages connecting to the API

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,15 +1,31 @@
 import { API_BASE_URL } from "./config";
 
 export async function loginRequest({ email, password }){
-    const response = await fetch(`${API_BASE_URL}/api/v1/auth/login`, {
+    const response = await fetch(`https://jun25-t2-task-tracker-api.onrender.com/api/v1/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({email, password})
     });
 
     const data = await response.json();
+    console.log(data);
     if(!response.ok) {
         throw new Error(data.error || data.name || data.message || 'Login failed.');
+    }
+
+    return data;
+}
+
+export async function signupRequest({ email, password, role }){
+        const response = await fetch(`https://jun25-t2-task-tracker-api.onrender.com/api/v1/auth/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({email, password, role})
+    });
+
+    const data = await response.json();
+    if(!response.ok) {
+        throw new Error(data.error || data.name || data.message || 'Registeration failed.');
     }
 
     return data;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,32 @@
 import { Link } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 
-export default function Navbar(){
-    const { isAuthenticated, role, logout } = useAuth();
+export default function Navbar() {
+  const { isAuthenticated, role, logout } = useAuth();
 
-    // const handleFakeLogin = () => {
-    //     login({ userId: '123', role: 'user', token: 'abcd' });
-    // };
+  // const handleFakeLogin = () => {
+  //     login({ userId: '123', role: 'user', token: 'abcd' });
+  // };
 
-    return(
-        <header>
-            <h1>Task Tracker</h1>
-            <nav>
-                {
-                !isAuthenticated ? (
-                    <button onClick={handleFakeLogin} type="button">Fake Login</button>
-                ): (
-                    <>
-                    <span>Logged in, ({role})</span>
-                    <button type="button" onClick={logout}>Logout</button>
-                    </>
-                )  
-                }
-                <Link to="/">Home</Link> {" "}
-                <Link to="/tasks">Tasks</Link> {" "}
-                <Link to="/login">Login</Link> {" "}
-                <Link to="/register">Register</Link> {" "}
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Task Tracker</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/tasks">Tasks</Link> |{" "}
+        {!isAuthenticated ? (
+          <>
+            <Link to="/login">Login</Link> |{" "}
+            <Link to="/register">Register</Link> |{" "}
+          </>
+        ) : (
+          <>
+            <span>Logged in, ({role})</span>{" "}
+            <button type="button" onClick={logout}>
+              Logout
+            </button>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
